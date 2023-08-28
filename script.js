3c74912eb4ac65cf57616b34ae17931eaa4edf66
Javier Rojas <javierrojas14.jr@gmail.com>
2023-08-28 14:35:21 -0400

    Formats document with prettier

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
 function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-
+  return firstNumber + secondNumber;
 }
 
 function subtract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-
+  return firstNumber - secondNumber;
 }
 
 function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-
+  return firstNumber * secondNumber;
 }
 
 function divide(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
-
+  return firstNumber / secondNumber;
 }
 
 function operate(operator, firstNumber, secondNumber) {
-    if (operator === '+') {
-        add(firstNumber, secondNumber);
-    } else if (operator === '-') {
-        subtract(firstNumber, secondNumber);
-    } else if (operator === '*') {
-        multiply(firstNumber, secondNumber);
-    } else if (operator === '/') {
-        divide(firstNumber, secondNumber);
-    } else {
-        return 'You must enter a valid operator';
-    }
-
+  if (operator === "+") {
+    add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    subtract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    multiply(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    divide(firstNumber, secondNumber);
+  } else {
+    return "You must enter a valid operator";
+  }
 }
-
-function bindButtons() {
-    const allButtons = document.querySelectorAll('button');
-    Array.from(allButtons, (element) => element.addEventListener('click', evaluateActionOfButton));
-}
-
 
 function evaluateActionOfButton(event) {
-    let buttonPressed = event.target;
-    let characterOfButton = buttonPressed.textContent;
+  let buttonPressed = event.target;
+  let characterOfButton = buttonPressed.textContent;
 
-    if (characterOfButton === 'AC') {
-        deleteContentOfVisor();
-    } else if (characterOfButton === '=') {
-        alert('Hay que hacer una operacion!');
-    } else if (characterOfButton === '?') {
-        //pass
-    } else {
-        sendValueToDisplay(characterOfButton);
-    }
+  if (characterOfButton === "AC") {
+    deleteContentOfVisor();
+  } else if (characterOfButton === "=") {
+    alert("Hay que hacer una operacion!");
+  } else if (characterOfButton === "?") {
+    //pass
+  } else {
+    sendValueToDisplay(characterOfButton);
+  }
 }
 
 function deleteContentOfVisor() {
-    let visorElement = document.querySelector('.results-visor');
-    visorElement.textContent = '';
+  let visorElement = document.querySelector(".results-visor");
+  visorElement.textContent = "";
 }
 
-
 function sendValueToDisplay(characterOfButton) {
-    let visorElement = document.querySelector('.results-visor');
-    visorElement.textContent += characterOfButton;
+  let visorElement = document.querySelector(".results-visor");
+  visorElement.textContent += characterOfButton;
+}
+
+function bindButtons() {
+  const allButtons = document.querySelectorAll("button");
+  Array.from(allButtons, (element) =>
+    element.addEventListener("click", evaluateActionOfButton)
+  );
 }
 
 bindButtons();
